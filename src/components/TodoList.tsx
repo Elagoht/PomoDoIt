292d23db3f561f01d0131a349af8a2af088112c9
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-07-20 15:32:12 +0300

    chore: delete todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react"
 import { IToDo } from "../types/states"
-import { SetTodoChecked, SetTodoSessions } from "../utils/states"
+import { RemoveCategory, RemoveTodo, SetTodoChecked, SetTodoSessions } from "../utils/states"
 import classNames from "classnames"
+import { X } from "lucide-react"
 
 interface TodoListProps {
   category: [string, IToDo[]]
@@ -13,9 +14,16 @@ const TodoList: FC<TodoListProps> = ({ category }) => {
   return <div>
     <div key={category[0]}>
       <h1 className="text-2xl">{category[0]}</h1>
-      <ol>
+      <div>
         {category[1].map((item, index) => (
-          <li key={item.todo} className="flex gap-2 p-4 border rounded">
+          <label
+            key={item.todo}
+            htmlFor={`todo-${index}`}
+            className={classNames({
+              "flex gap-2 p-4 border rounded cursor-pointer": true,
+              "line-through": item.checked
+            })}
+          >
             <input
               type="checkbox"
               id={`todo-${index}`}
@@ -25,20 +33,29 @@ const TodoList: FC<TodoListProps> = ({ category }) => {
               }}
             />
 
-            <label
-              htmlFor={`todo-${index}`}
-              className={classNames({ "line-through": item.checked })}
-            > {item.todo}</label>
+            <div className="flex-1 line-clamp-1">
+              {item.todo}
+            </div>
 
             <input
               value={item.session}
-              onChange={(event) => SetTodoSessions([category[0], index, parseInt(event.currentTarget.value)])}
+              onChange={(event) =>
+                SetTodoSessions([category[0], index, parseInt(event.currentTarget.value)])
+              }
               type="number"
               min={1}
-              className="w-16" />
-          </li>
+              className="w-16"
+            />
+
+            <button
+              onClick={() => RemoveTodo([category[0], index])}
+            >
+              <X className="text-red-600 hover:text-red-800" />
+            </button>
+
+          </label>
         ))}
-      </ol>
+      </div>
     </div>
     <pre>
       {JSON.stringify(category[1], null, 2)}
